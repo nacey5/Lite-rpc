39d6d193945de9a011aaf894489fd2a86d9a8fc5
dahuang <1160124552@qq.com>
2023-08-11 11:19:59 +0800

    初始化搭建

---
+++ src/main/java/com/hzh/minirpc/MiniRpcApplication.java
@@ -0,0 +1,13 @@
+package com.hzh.minirpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzh/minirpc/MiniRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzh.minirpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
